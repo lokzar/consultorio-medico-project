b87f2bf586600dbce3859e600d25bb0f49ca8622
lokzar <motart2505@gmail.com>
2022-05-05 20:46:03 -0500

    ojalá este si les guste jaja

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const abletoappointment = require("../middleware/abletoappointment");
 const Appointment = require("../models/Appointment.model");
 const Service = require("../models/Service.model");
+const { populate } = require("../models/User.model");
 const User = require("../models/User.model");
 
 
@@ -27,13 +28,10 @@ router.post("/new/appointment", (req, res, next) => {
 
     const appointment = {...req.body, idUser: req.session?.user?._id}
     const userId = req.session?.user?._id
-
-    console.log(appointment)
     Appointment.create(appointment)
         .then(appointment => {
             Service.findById(appointment.idService)
                 .then(serviceFound => {
-                        console.log(appointment)
                         res.render("appointment/appointment-details", {appointment, serviceFound})
                     })
                     .catch(error => console.log(error))
@@ -46,15 +44,42 @@ router.post("/new/appointment", (req, res, next) => {
 //revisar todas las citas
 router.get('/appointment/list',(req,res)=>{
     Appointment.find()
+    .populate("idUser")
     .then(registeredAppointments=>{
         const isAdmin = req.session?.user?.profile=="admin" ? true : false
         const isLoggedIn = req.session?.user ? true :false
         const username = req.session?.user?.username
         const avatar=req.session?.user?.avatar
-        console.log(registeredAppointments)
-        res.render('appointment/appointment-list', {data: registeredAppointments,isAdmin,isLoggedIn,username,avatar} )
+
+        res.render('appointment/appointment-list', {data:registeredAppointments,isAdmin,isLoggedIn,username,avatar} )
     })
     .catch(err=>(console.log("Error en find: ",err)))
+})
+
+//rechazar citas
+
+router.get("/appointment/:id/edit",(req,res,next)=>{
+    const isAdmin = req.session?.user?.profile=="admin" ? true : false
+    const isLoggedIn = req.session?.user ? true :false
+    const username = req.session?.user?.username
+    const avatar=req.session?.user?.avatar
+    const{id}=req.params
+    Appointment.findById(id)
+    .then(appointment=>{
+        res.render("appointment/appointment-edit",{appointment,isAdmin,isLoggedIn,username,avatar})
+    })
+    .catch(err=>console.log(err))
+})
+
+
+router.post("/appointment/:id/delete", (req,res,next)=>{
+    const {id} = req.params
+    Appointment.findByIdAndDelete(id)
+    .then(()=>{
+        console.log("cita borrada")
+        res.redirect("/appointment/list")
+    })
+    .catch(err=>console.log(err))
 })
 
 
